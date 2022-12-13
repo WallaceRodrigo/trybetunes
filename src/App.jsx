452f5cc8561014dc262bf0de10188dc3da90df69
Wallace <wallacerodrigo2004@gmail.com>
2022-12-13 20:10:26 -0300

    2 login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -7,46 +7,27 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
-import Loading from './components/Loading';
 
 const localStorageFull = JSON.parse(localStorage.getItem('user'));
 
 class App extends React.Component {
   state = {
     login: '',
-    loginTrue: false,
     ...localStorageFull,
   };
-
-  componentDidUpdate() {
-    const { name } = this.state;
-    if (name !== undefined) {
-      this.setState({ loginTrue: true });
-    }
-  }
 
   handleChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
-    this.setState({
-      [name]: value,
-    }, this.saveButtonValidation);
+    this.setState({ [name]: value });
   };
-
-  // loginValidation = () => {
-  //   const localStorageFull = !JSON.parse(localStorage.getItem('user')) === false;
-  //   if (localStorageFull) {
-  //     this.setState({ loginTrue: true });
-  //   }
-  // };
 
   render() {
     const {
       login,
-      loginTrue,
     } = this.state;
-    console.log(localStorageFull);
+
     return (
       <BrowserRouter>
         <div>
@@ -56,14 +37,14 @@ class App extends React.Component {
               exact
               path="/"
               render={ (p) => (
-                <Login { ...p } inputChange={ this.handleChange } loginName={ login } />
+                <Login
+                  { ...p }
+                  inputChange={ this.handleChange }
+                  loginName={ login }
+                />
               ) }
             />
-            <Route
-              exact
-              path="/search"
-              render={ () => (!loginTrue ? <Loading /> : <Search />) }
-            />
+            <Route exact path="/search" component={ Search } />
             <Route exact path="/album/:id" component={ Album } />
             <Route exact path="/favorites" component={ Favorites } />
             <Route exact path="/profile" component={ Profile } />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,59 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { RouterPropTypes } from 'react-router';
 import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends Component {
-  onClick = () => {
-    const { loginName, history } = this.props;
+  state = {
+    isLoading: false,
+  };
 
-    createUser({ name: loginName });
+  onClick = async () => {
+    const { loginName, history } = this.props;
+    this.setState({ isLoading: true });
+    await createUser({ name: loginName });
 
     return history.push('/search');
   };
 
   render() {
     const { inputChange, loginName } = this.props;
+    const { isLoading } = this.state;
 
     const three = 3;
     const loginButtonValidation = loginName.length >= three;
 
-    // console.log(this.onClick());
     return (
-      <div data-testid="page-login">
-        <form>
-          <label htmlFor="login">
-            Username
-            <input
-              type="text"
-              name="login"
-              data-testid="login-name-input"
-              onChange={ inputChange }
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="login-submit-button"
-            disabled={ !loginButtonValidation }
-            onClick={ () => this.onClick() }
-          >
-            Entrar
-          </button>
-        </form>
+      <div>
+        {
+          isLoading
+            ? <Loading />
+            : (
+              <div data-testid="page-login">
+                <form>
+                  <label htmlFor="login">
+                    Username
+                    <input
+                      type="text"
+                      name="login"
+                      data-testid="login-name-input"
+                      onChange={ inputChange }
+                    />
+                  </label>
+                  <button
+                    type="button"
+                    data-testid="login-submit-button"
+                    disabled={ !loginButtonValidation }
+                    onClick={ () => this.onClick() }
+                  >
+                    Entrar
+                  </button>
+                </form>
+              </div>
+            )
+        }
       </div>
+
     );
   }
 }
